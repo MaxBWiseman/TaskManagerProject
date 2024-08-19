fde007fd1d1dd3521f45060e37918b0163ba7b32
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-19 15:25:08 +0000

    chore: Update home route to render tasks.html template

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,4 +6,6 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
+# The home route is the default route that renders the tasks.html template.
+# this page will be displayed always when the user visits the website first.
