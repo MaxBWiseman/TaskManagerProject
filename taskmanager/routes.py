26e4f1724aecfd9dd5bdacad02041423d49fc605
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-19 15:53:02 +0000

    chore: Add routes for categories and add_category pages

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,15 @@ from taskmanager.models import Category, Task
 
 
 @app.route("/")
-def home():
+def home():# resieves the home() function from clicking nav links
     return render_template("tasks.html")
 # The home route is the default route that renders the tasks.html template.
 # this page will be displayed always when the user visits the website first.
+
+@app.route("/categories")
+def categories():# resieves the categories() function from clicking nav links
+    return render_template("categories.html")
+
+@app.route("/add_category", methods=["GET", "POST"])# when user submits the form, the data is sent to the database
+def add_category():# resieves the add_category() function from clicking nav links
+    return render_template("add_category.html")
