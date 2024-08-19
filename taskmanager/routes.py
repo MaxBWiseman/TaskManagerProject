26742d93974fecf8e30a7f5111d66887652f30ad
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-19 16:06:25 +0000

    chore: Update add_category route to redirect after form submission

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -17,7 +17,13 @@ def categories():# resieves the categories() function from clicking nav links
 @app.route("/add_category", methods=["GET", "POST"])# when user submits the form, the data is sent to the database
 def add_category():# resieves the add_category() function from clicking nav links
     if request.method == "POST":
-        category = Category(name=request.form.get("category_name"))
+        category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
+    """
+When a user clicks the "Add Category" button, this will use the "GET" method and render the 'add_category' template.
+Once they submit the form, this will call the same function, but will check if the request
+being made is a “POST“ method, which posts data somewhere, such as a database.
+    """
