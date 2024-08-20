24c4385e2efa62c9e7172aa0956e261ce70d0f44
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-20 19:05:19 +0000

    testing

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from taskmanager import app, db
-
-with app.app_context():
-    db.create_all()
-    
-# This script creates the database tables by importing the app and db objects from the taskmanager
-# package and calling the create_all() method on the db object within the app context. The app context
-# is necessary to ensure that the app and db objects are properly initialized before creating the tables.
-# This script can be run from the command line using the following command: python create_db.py
